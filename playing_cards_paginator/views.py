41f843f94a4a17040f8f3dacee6592b2f064b61b
ferronsnicola <ferronsnicola@gmail.com>
2023-03-12 04:15:47 +0100

    #dev #frontend basic view to let upload single file and dir of files

--- playing_cards_paginator/views.py
+++ playing_cards_paginator/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from .models import BackFile, FrontFiles
+from .forms import DeckForm
 
-# Create your views here.
+
+def file_loader(request):
+    message = 'Upload as many files as you want!'
+    # Handle file upload
+    if request.method == 'POST':
+        form = DeckForm(request.POST, request.FILES)
+        if form.is_valid():
+            group_name = request.POST['name']
+            newdoc = BackFile(back=request.FILES['back'])
+            newdoc.save()
+
+            for front in request.FILES.getlist('fronts'):
+                newdoc = FrontFiles(front=front)
+                newdoc.save()
+
+
+            # Redirect to the document list after POST
+            return redirect('file_loader')
+        else:
+            message = 'The form is not valid. Fix the following error:'
+    else:
+        form = DeckForm()  # An empty, unbound form
+
+    # Load documents for the list page
+    documents = BackFile.objects.all()
+
+    # Render list page with the documents and the form
+    context = {'documents': documents, 'form': form, 'message': message}
+    return render(request, 'list.html', context)
+
