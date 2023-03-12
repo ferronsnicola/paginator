f2c9bbd35ce4df04e234982ed9ad013591f3cf0e
ferronsnicola <ferronsnicola@gmail.com>
2023-03-12 04:12:04 +0100

    #dev #frontend setup for django frontend

--- playing_cards_paginator/admin.py
+++ playing_cards_paginator/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from django.apps import apps
 
 # Register your models here.
+for model in apps.get_app_config('playing_cards_paginator').get_models():
+    admin.site.register(model)
