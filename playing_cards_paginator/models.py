8ed073be9a34a4814702e68a6e3b81b32c31f0e4
ferronsnicola <ferronsnicola@gmail.com>
2023-03-12 04:13:47 +0100

    #dev #frontend basic django models

--- playing_cards_paginator/models.py
+++ playing_cards_paginator/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+
 # Create your models here.
+class BackFile(models.Model):
+    back = models.FileField(upload_to='documents/backs')
+
+
+class FrontFiles(models.Model):
+    front = models.FileField(upload_to='documents/fronts')
+
+
