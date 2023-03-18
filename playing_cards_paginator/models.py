b3fc2504816efbb6b212179b2ba54998162e47b7
ferronsnicola <ferronsnicola@gmail.com>
2023-03-18 14:21:48 +0100

    #dev #frontend views.py models.py session management

--- playing_cards_paginator/models.py
+++ playing_cards_paginator/models.py
@@ -3,19 +3,21 @@ import json
 
 
 def dynamic_path(instance, filename):
-    return f'documents/{instance.base_dir}/{instance.group_name}/{filename}'
+    return f'documents/{instance.session_id}/{instance.base_dir}/{instance.group_name}/{filename}'
 
 
 
 # Create your models here.
 class BackFile(models.Model):
     base_dir = 'backs'
+    session_id = models.CharField(max_length=255)
     group_name = models.CharField(max_length=255)
     back = models.FileField(upload_to=dynamic_path)
 
 
 class FrontFiles(models.Model):
     base_dir = 'fronts'
+    session_id = models.CharField(max_length=255)
     group_name = models.CharField(max_length=255)
     front = models.FileField(upload_to=dynamic_path)
 
--- playing_cards_paginator/views.py
+++ playing_cards_paginator/views.py
@@ -7,22 +7,25 @@ from . import cards_formats
 from os.path import join
 import os
 from django.views.static import serve
+from django.http import HttpRequest
 
 
-def file_loader(request):
+def file_loader(request: HttpRequest):
     message = 'Upload as many files as you want!'
+    if request.session.session_key is None:
+        request.session.save()
+    session_key = request.session.session_key
 
     # Handle file upload
-    print(request.POST)
     if request.method == 'POST' and 'upload' in request.POST:
         form = DeckForm(request.POST, request.FILES)
         if form.is_valid():
             group_name = request.POST['name']
-            newdoc = BackFile(back=request.FILES['back'], group_name=group_name)
+            newdoc = BackFile(back=request.FILES['back'], group_name=group_name, session_id=session_key)
             newdoc.save()
 
             for front in request.FILES.getlist('fronts'):
-                newdoc = FrontFiles(front=front, group_name=group_name)
+                newdoc = FrontFiles(front=front, group_name=group_name, session_id=session_key)
                 newdoc.save()
 
 
@@ -31,12 +34,8 @@ def file_loader(request):
         else:
             message = 'The form is not valid. Fix the following error:'
     elif request.method == 'GET' and 'confirm&download' in request.GET:
-        print('DOWNLOADING FILES!')
-
-        print(request.GET)
-
-        plotter_format = request.GET.get('plotter_formats', 'AAA')
-        if plotter_format != 'AAA':
+        plotter_format = request.GET.get('plotter_formats', None)
+        if plotter_format is not None:
 
             cf = cards_formats.get_h_w_mm_from_format(request.GET.get('cards_formats', 'AAA'))
 
@@ -54,7 +53,7 @@ def file_loader(request):
         form = DeckForm()  # An empty, unbound form
 
     # Load documents for the list page
-    documents = BackFile.objects.all()
+    documents = BackFile.objects.filter(session_id=session_key)
 
     # Render list page with the documents and the form
     context = {'documents': documents, 'form': form, 'message': message}
