3be0f250f58fc58dad63ac43e16b53dfe27abf35
ferronsnicola <ferronsnicola@gmail.com>
2023-05-26 22:55:31 +0200

    #dev #bugfix models refactor fixed issue: it should be impossible to upload a deck with same name as an existing one

--- playing_cards_paginator/models.py
+++ playing_cards_paginator/models.py
@@ -3,24 +3,12 @@ import json
 
 
 def dynamic_path(instance, filename):
-    return f'documents/{instance.session_id}/{instance.base_dir}/{instance.group_name}/{filename}'
+    return f'documents/{instance.session_id}/{instance.base_dir}/{instance.deck_name}/{filename}'
 
 
-
-# Create your models here.
-class BackFile(models.Model):
-    base_dir = 'backs'
+class CardFile(models.Model):
+    base_dir = models.CharField(max_length=255)
     session_id = models.CharField(max_length=255)
-    group_name = models.CharField(max_length=255)
-    back = models.FileField(upload_to=dynamic_path)
+    deck_name = models.CharField(max_length=255)
+    file = models.FileField(upload_to=dynamic_path)
     short_name = models.CharField(max_length=255)
-
-
-class FrontFiles(models.Model):
-    base_dir = 'fronts'
-    session_id = models.CharField(max_length=255)
-    group_name = models.CharField(max_length=255)
-    front = models.FileField(upload_to=dynamic_path)
-    short_name = models.CharField(max_length=255)
-
-
--- playing_cards_paginator/views.py
+++ playing_cards_paginator/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import redirect, render
-from .models import BackFile, FrontFiles
+from .models import CardFile
 from .forms import DeckForm
 from django.conf import settings
 from . import cards_placer
@@ -22,34 +22,34 @@ def file_loader(request: HttpRequest):
     if request.method == 'POST' and 'upload' in request.POST:
         form = DeckForm(request.POST, request.FILES)
         if form.is_valid():
-            group_name = request.POST['name']
-            if 'back' in request.FILES and 'fronts' in request.FILES:
-                newdoc = BackFile(back=request.FILES['back'], group_name=group_name, session_id=session_key, short_name=f'{group_name}/back')
+            deck_name = request.POST['name']
+            if CardFile.objects.filter(deck_name=deck_name).exists():
+                message_up = f'\nThe deck name {deck_name} already exists, change it!'
+            elif 'back' in request.FILES and 'fronts' in request.FILES:
+                newdoc = CardFile(base_dir='backs', file=request.FILES['back'], deck_name=deck_name, session_id=session_key, short_name=f'{deck_name}/back')
                 newdoc.save()
 
                 for front in request.FILES.getlist('fronts'):
-                    newdoc = FrontFiles(front=front, group_name=group_name, session_id=session_key, short_name=f'{group_name}/front')
+                    newdoc = CardFile(base_dir='fronts', file=front, deck_name=deck_name, session_id=session_key, short_name=f'{deck_name}/front')
                     newdoc.save()
 
-
-            # Redirect to the document list after POST
-            return redirect('file_loader')
+                # Redirect to the document list after POST
+                return redirect('file_loader')
         else:
             message_up = 'The form is not valid. Fix the following error:'
 
     elif request.method == 'POST' and 'Delete' in request.POST.values():
-        group_to_delete = ''
+        deck_to_delete = ''
         for k in request.POST.keys():
             if request.POST[k] == 'Delete':
-                group_to_delete = k
-        print(group_to_delete)
-        message_up += f' {group_to_delete} has been deleted!'
-        BackFile.objects.filter(group_name=group_to_delete).delete()
-        FrontFiles.objects.filter(group_name=group_to_delete).delete()
+                deck_to_delete = k
+        print(deck_to_delete)
+        message_up += f' {deck_to_delete} has been deleted!'
+        CardFile.objects.filter(deck_name=deck_to_delete).delete()
         session_dir = join(settings.MEDIA_ROOT, 'documents', session_key)
 
-        shutil.rmtree(join(session_dir, 'backs', group_to_delete))
-        shutil.rmtree(join(session_dir, 'fronts', group_to_delete))
+        shutil.rmtree(join(session_dir, 'backs', deck_to_delete))
+        shutil.rmtree(join(session_dir, 'fronts', deck_to_delete))
 
         form = DeckForm()
 
@@ -87,7 +87,6 @@ def file_loader(request: HttpRequest):
         if not cards_placer.check_consistency(cards_size=cards_width, pad=pad, bg_size=plotter_width):
             error_message += 'Plotter Width must be greater than Cards Width + 2 * Padding. '
             logic_error = True
-        # if get_spacing(plotter_height, cards_height)
         
 
         if not logic_error:
@@ -107,8 +106,8 @@ def file_loader(request: HttpRequest):
         form = DeckForm()  # An empty, unbound form
 
     # Load documents for the list page
-    backs = BackFile.objects.filter(session_id=session_key)
-    fronts = FrontFiles.objects.filter(session_id=session_key)
+    backs = CardFile.objects.filter(session_id=session_key, base_dir='backs')
+    fronts = CardFile.objects.filter(session_id=session_key, base_dir='fronts')
 
     already_checked = set()
     filtered_fronts = []
@@ -123,6 +122,6 @@ def file_loader(request: HttpRequest):
 
     # Render list page with the documents and the form
     context = {'backs_fronts': backs_fronts, 'form': form, 'message_up': message_up, 'message_down': message_down}
+    print(message_up)
     print(backs)
     return render(request, 'main_page.html', context)
-
